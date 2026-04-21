32dea92f1662e5173c8e7dd8f8e9db93c488a43e
Tyler Shetrompf <tyler@shetrompf.com>
2026-04-21 12:44:56 -0400

    added htmlnode and tests

---
+++ src/htmlnode.py
@@ -0,0 +1,34 @@
+
+
+class HTMLNode():
+
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        
+        self.tag = tag # string for html tag name
+        self.value = value # string for value of html tag
+        self.children = children # list of htmlnode child objects
+        self.props = props # dict of key value pairs representing html attributes
+
+    def __repr__(self):
+        print("**********Debug**********")
+        print(f'\ntag: {self.tag}\n value: {self.value}\n children: {self.children}\n props: {self.props}\n')
+        print("****** End of Debug *****")
+
+
+    def to_html(self):
+        
+        raise NotImplementedError
+    
+    def props_to_html(self):
+
+        ret_string = ""
+
+        if self.props == None or len(self.props) == 0:
+
+            return ""
+
+        for prop in self.props:
+
+            ret_string += f' {prop}="{self.props[prop]}"'
+        
+        return ret_string
